daef92686ea3fabd5db8b7e507e261773c3f8269
Chebykin Alexandr <vx@inbox.ru>
2024-04-25 13:50:19 +0300

    add all code

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import random
 
 pygame.init()
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 1024
+SCREEN_HEIGHT = 768
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Игра Тир")
@@ -12,16 +12,26 @@ icon = pygame.image.load("img/1.png")
 pygame.display.set_icon(icon)
 
 target_image = pygame.image.load("img/2.png")
-target_width = 50
-target_height = 50
+target_width = 100
+target_height = 100
 
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
-target_Y = random.randint(0, SCREEN_HEIGHT - target_height)
+target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
 color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
 running = True
 while running:
-    pass
+    screen.fill(color)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if (target_x < mouse_x < target_x + target_width) and (target_y < mouse_y < target_y + target_height):
+                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+    screen.blit(target_image, (target_x, target_y))
+    pygame.display.update()
 
 pygame.quit()
